8459d16bed5ddd0525663335fa13cd5abcce3d04
Lapi <tyoutugai.7@gmail.com>
2021-05-27 03:31:42 +0900

    fix format

--- src/Context.tsx
+++ src/Context.tsx
@@ -2,6 +2,6 @@ import { createContext } from "react"
 
 export const TokenContext = createContext({
   token: "",
-// eslint-disable-next-line
+  // eslint-disable-next-line
   setToken: (_: string) => {},
 })
