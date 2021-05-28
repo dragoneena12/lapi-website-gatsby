a38a456656bd986ff942952d45d7f944158f9e5b
lapi <tyoutugai.7@gmail.com>
2021-05-29 08:54:16 +0900

    fix spinner

--- src/components/Hotel/Hotel.tsx
+++ src/components/Hotel/Hotel.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useContext } from "react"
 import { useAuth0 } from "@auth0/auth0-react"
-import { Spinner } from "css-spinners-react"
 
 import Heading from "../common/Heading"
 import Fuwa from "../utils/Fuwa"
 import { TokenContext } from "../../Context"
+import { SpinnerLoader } from "../common/Spinner.module.scss"
 import Description from "./Description"
 import Profile from "./Profile"
 import HotelList from "./HotelList"
@@ -41,7 +41,7 @@ const Hotel: React.FC = () => {
       <Fuwa>
         {isLoading ? (
           <div className={Loading}>
-            <Spinner />
+            <div className={SpinnerLoader} />
           </div>
         ) : user ? (
           <Profile user={user} />
