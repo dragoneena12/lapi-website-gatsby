f806441a8a55b12980b74da076e499e67d7cbe09
lapi <tyoutugai.7@gmail.com>
2021-06-18 00:30:01 +0900

    remove fuwa

--- src/components/Hotel/HotelDetail.tsx
+++ src/components/Hotel/HotelDetail.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Heading from "../common/Heading"
-import Fuwa from "../utils/Fuwa"
 import { Hotel } from "../../generated/graphql"
 import { Ranks } from "./Profile"
 import {
@@ -23,61 +22,51 @@ const HotelDetail: React.FC<Props> = ({ hotel }) => {
     <div className={Container}>
       <Heading>{hotel.name}</Heading>
       <div className={BoxWrapper}>
-        <Fuwa>
-          <div className={Box}>
-            <h2 className={Text}>{Ranks[0].name}</h2>
-            <h3 className={Text}>{`宿泊数 ${Ranks[0].count} 泊以上`}</h3>
-            <ul className={List}>
-              {hotel.carbonAwards.map(el => (
-                <li key={el}>{el}</li>
-              ))}
-            </ul>
-          </div>
-        </Fuwa>
-        <Fuwa>
-          <div className={Box}>
-            <h2 className={Text}>{Ranks[1].name}</h2>
-            <h3 className={Text}>{`宿泊数 ${Ranks[1].count} 泊以上`}</h3>
-            <ul className={List}>
-              {hotel.fullereneAwards.map(el => (
-                <li key={el}>{el}</li>
-              ))}
-            </ul>
-          </div>
-        </Fuwa>
-        <Fuwa>
-          <div className={Box}>
-            <h2 className={Text}>{Ranks[2].name}</h2>
-            <h3 className={Text}>{`宿泊数 ${Ranks[2].count} 泊以上`}</h3>
-            <ul className={List}>
-              {hotel.carbonNanotubeAwards.map(el => (
-                <li key={el}>{el}</li>
-              ))}
-            </ul>
-          </div>
-        </Fuwa>
-        <Fuwa>
-          <div className={Box}>
-            <h2 className={Text}>{Ranks[3].name}</h2>
-            <h3 className={Text}>{`宿泊数 ${Ranks[3].count} 泊以上`}</h3>
-            <ul className={List}>
-              {hotel.grapheneAwards.map(el => (
-                <li key={el}>{el}</li>
-              ))}
-            </ul>
-          </div>
-        </Fuwa>
-        <Fuwa>
-          <div className={Box}>
-            <h2 className={Text}>{Ranks[4].name}</h2>
-            <h3 className={Text}>{`宿泊数 ${Ranks[4].count} 泊以上`}</h3>
-            <ul className={List}>
-              {hotel.diamondAwards.map(el => (
-                <li key={el}>{el}</li>
-              ))}
-            </ul>
-          </div>
-        </Fuwa>
+        <div className={Box}>
+          <h2 className={Text}>{Ranks[0].name}</h2>
+          <h3 className={Text}>{`宿泊数 ${Ranks[0].count} 泊以上`}</h3>
+          <ul className={List}>
+            {hotel.carbonAwards.map(el => (
+              <li key={el}>{el}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={Box}>
+          <h2 className={Text}>{Ranks[1].name}</h2>
+          <h3 className={Text}>{`宿泊数 ${Ranks[1].count} 泊以上`}</h3>
+          <ul className={List}>
+            {hotel.fullereneAwards.map(el => (
+              <li key={el}>{el}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={Box}>
+          <h2 className={Text}>{Ranks[2].name}</h2>
+          <h3 className={Text}>{`宿泊数 ${Ranks[2].count} 泊以上`}</h3>
+          <ul className={List}>
+            {hotel.carbonNanotubeAwards.map(el => (
+              <li key={el}>{el}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={Box}>
+          <h2 className={Text}>{Ranks[3].name}</h2>
+          <h3 className={Text}>{`宿泊数 ${Ranks[3].count} 泊以上`}</h3>
+          <ul className={List}>
+            {hotel.grapheneAwards.map(el => (
+              <li key={el}>{el}</li>
+            ))}
+          </ul>
+        </div>
+        <div className={Box}>
+          <h2 className={Text}>{Ranks[4].name}</h2>
+          <h3 className={Text}>{`宿泊数 ${Ranks[4].count} 泊以上`}</h3>
+          <ul className={List}>
+            {hotel.diamondAwards.map(el => (
+              <li key={el}>{el}</li>
+            ))}
+          </ul>
+        </div>
       </div>
       <Link to="/hotel" className={Button}>
         戻る
