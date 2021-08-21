23d52c9a125842d524c43c8d15231324c9f3bcb8
lapi <tyoutugai.7@gmail.com>
2021-08-21 22:52:48 +0900

    fix stayhistory formatting

--- src/components/Hotel/StayHistory.tsx
+++ src/components/Hotel/StayHistory.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+import { format } from "date-fns"
+import ja from "date-fns/locale/ja"
 
 import { Stay } from "../../generated/graphql"
 import { Container, BoxContainer, Box, Button } from "./StayHistory.module.scss"
@@ -13,8 +15,12 @@ const StayHistory: React.FC<{ stays: Partial<Stay>[] }> = ({ stays }) => (
           <div className={Box} key={stay.id}>
             <h2>{stay.hotel?.name}</h2>
             {/* <p>{`宿泊ID：${stay.id}`}</p> */}
-            <p>{`チェックイン：${new Date(stay.checkin).toDateString()}`}</p>
-            <p>{`チェックアウト：${new Date(stay.checkout).toDateString()}`}</p>
+            <p>{`IN: ${format(new Date(stay.checkin), "Yo MMM do eeee", {
+              locale: ja,
+            })}`}</p>
+            <p>{`OUT: ${format(new Date(stay.checkout), "Yo MMM do eeee", {
+              locale: ja,
+            })}`}</p>
           </div>
         ))}
       </div>
