209760f6b1f8bab969ca7529143a0fcca93cd40b
Lapi <tyoutugai.7@gmail.com>
2021-09-10 18:39:36 +0900

    fix ssr

--- src/components/Games/Escape01.tsx
+++ src/components/Games/Escape01.tsx
@@ -3,25 +3,28 @@ import Unity, { UnityContent } from "react-unity-webgl"
 
 import * as classes from "./Escape01.module.scss"
 
-const unityContent = new UnityContent(
-  "/unity/escape01/Build.json",
-  "/unity/escape01/UnityLoader.js"
-)
-
 const Escape01: React.FC = () => {
-  return (
-    <div className={classes.Escape01 + " mx-auto"}>
-      <Unity unityContent={unityContent} />
-      <div className={classes.Explanation}>
-        <h3>Escape01</h3>
-        <p>
-          Unityで初めて作った謎の脱出ゲームです。
-          <br />
-          PCのみ対応。マウスだけで操作します。
-        </p>
+  if (typeof window !== `undefined`) {
+    const unityContent = new UnityContent(
+      "/unity/escape01/Build.json",
+      "/unity/escape01/UnityLoader.js"
+    )
+    return (
+      <div className={classes.Escape01 + " mx-auto"}>
+        <Unity unityContent={unityContent} />
+        <div className={classes.Explanation}>
+          <h3>Escape01</h3>
+          <p>
+            Unityで初めて作った謎の脱出ゲームです。
+            <br />
+            PCのみ対応。マウスだけで操作します。
+          </p>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return <div>only work on browser</div>
+  }
 }
 
 export default Escape01
